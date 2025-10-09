ad0806472fd2a9252cf8ac9bd310bba3d4ff27e3
RaikNorri <ikolpokis@gmail.com>
2025-10-09 23:38:01 +0300

    Game end

--- Assets/Assets/Scripts/Game.cs
+++ Assets/Assets/Scripts/Game.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 
 public class Game:MonoBehaviour
@@ -22,7 +24,7 @@ public class Game:MonoBehaviour
 
     private void Start()
     {
-        
+        state = new MenuGameState(this);
     }
 
     public void ChangeState(GameStatesType stateType)
@@ -30,16 +32,24 @@ public class Game:MonoBehaviour
         switch (stateType)
         {
             case GameStatesType.StartGame:
+                state.Exit();
                 state = new StartGameState(this);
+                state.Enter();
                 break;
             case GameStatesType.DieGame:
+                state.Exit();
                 state = new StartGameState(this);
+                state.Enter();
                 break;
             case GameStatesType.WinGame:
+                state.Exit();
                 state = new StartGameState(this);
+                state.Enter();
                 break;
             case GameStatesType.EndGame:
+                state.Exit();
                 state = new StartGameState(this);
+                state.Enter();
                 break;
         }
     }
@@ -100,7 +110,25 @@ public class UI:MonoBehaviour
     }
 
 
+    public void StartMenu()
+    {
 
+    }
+
+    public void DeathPanel()
+    {
+
+    }
+
+    public void WinPanel()
+    {
+
+    }
+
+    public void EndGamePanel()
+    {
+
+    }
 
 
     void SetText(TextMeshProUGUI text, string newText)
@@ -125,15 +153,42 @@ public abstract class GameState
     public abstract void Exit();
 }
 
+
+public static class Signal
+{
+    public static bool StartPressed = false;
+}
 public class MenuGameState:GameState
 {
+    Game game;
+    public MenuGameState(Game _game)
+    {
+        game = _game;
+    }
+
+    Coroutine menuCoroutine;
     public override void Action()
     {
+        Signal.StartPressed = false;
+        menuCoroutine = game.StartCoroutine(Menu());
 
+    }
+
+    IEnumerator Menu()
+    {
+        UI.Instance.StartMenu();
+
+        while (!Signal.StartPressed)
+        {
+            yield return null;
+        }
+
+        game.ChangeState(GameStatesType.StartGame);
     }
     public override void Exit()
     {
-
+        if(menuCoroutine!=null)
+            game.StopCoroutine(menuCoroutine);
     }
 }
 
@@ -147,8 +202,8 @@ public class StartGameState:GameState
 
     public override void Action()
     {
-        
-        GameObject.Instantiate(game.scenePrefab);
+        game.StartCoroutine(StartGame());
+
         
         
         
@@ -162,6 +217,14 @@ public class StartGameState:GameState
 
     }
 
+    IEnumerator StartGame()
+    {
+
+
+
+        GameObject.Instantiate(game.scenePrefab);
+        yield return null;
+    }
 
 
 
@@ -171,6 +234,72 @@ public class StartGameState:GameState
     }
 }
 
+public class WinGameState : GameState
+{
+    Game game;
+    public WinGameState(Game _game)
+    {
+        game = _game;
+    }
+
+    public override void Action()
+    {
+        Timer.Instance.StopTimer();
+        UI.Instance.DeathPanel();
+        game.ChangeState(GameStatesType.EndGame);
+    }
+
+    public override void Exit()
+    {
+        
+    }
+}
+public class DieGameState : GameState
+{
+    Game game;
+    public DieGameState(Game _game)
+    {
+        game = _game;
+    }
+
+    public override void Action()
+    {
+        Timer.Instance.StopTimer();
+        UI.Instance.DeathPanel();
+        game.ChangeState(GameStatesType.EndGame);
+
+    }
+    
+
+    public override void Exit()
+    {
+        
+    }
+}
+
+public class EndGameState : GameState
+{
+    Game game;
+    public EndGameState(Game _game)
+    {
+        game= _game;
+    }
+    public override void Action()
+    {
+        game.StartCoroutine(EndGame());
+    }
+
+    IEnumerator EndGame()
+    {
+        yield return new WaitForSeconds(5f);
+        UI.Instance.EndGamePanel();
+    }
+
+    public override void Exit()
+    {
+
+    }
+}
 public enum GameStatesType
 {
     StartGame,
